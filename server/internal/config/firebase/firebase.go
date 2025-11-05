b20c2cfd7426caf6d9e91b07886bb17a10ddb1ab
jose vitor <josevitorrodrigues17@gmail.com>
2025-11-05 14:28:49 -0300

    chore: improve initialization safety and error handling

--- server/internal/config/firebase/firebase.go
+++ server/internal/config/firebase/firebase.go
@@ -2,26 +2,51 @@ package firebase
 
 import (
 	"context"
+	"errors"
 	"os"
+	"sync"
 
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
 	"google.golang.org/api/option"
 )
 
-var firebaseAuth *auth.Client
+var (
+	firebaseAuth *auth.Client
+	once         sync.Once
+	initErr      error
+)
 
 func InitFirebase() error {
-	opt := option.WithCredentialsFile(os.Getenv("FIREBASE_CREDENTIALS_PATH"))
-	app, err := firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		return err
-	}
+	once.Do(func() {
+		credsPath := os.Getenv("FIREBASE_CREDENTIALS_PATH")
+		if credsPath == "" {
+			initErr = errors.New("FIREBASE_CREDENTIALS_PATH not set")
+			return
+		}
 
-	firebaseAuth, err = app.Auth(context.Background())
-	return err
+		opt := option.WithCredentialsFile(credsPath)
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			initErr = err
+			return
+		}
+
+		firebaseAuth, initErr = app.Auth(context.Background())
+	})
+
+	return initErr
 }
 
+
 func VerifyToken(idToken string) (*auth.Token, error) {
+	if firebaseAuth == nil {
+		return nil, errors.New("firebase auth client not initialized")
+	}
 	return firebaseAuth.VerifyIDToken(context.Background(), idToken)
+}
+
+
+func GetAuthClient() *auth.Client {
+	return firebaseAuth
 }
